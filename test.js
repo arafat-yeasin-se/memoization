ccd237d4886236f02cc7a281fe8d59dcc5d4549e
yeasin-arafat <rafan.arafat07@gmail.com>
2018-08-20 13:52:41 +0600

    Added some comments to clarify the steps taken to test the cases. Test suited added to do test memoize function with sinon fake timers.

--- test.js
+++ test.js
@@ -6,9 +6,9 @@ const sinon = require('sinon');
 
 describe('memoization', function () {
     //Test Case 1
-    it('should memoize function result', () =>{
+    it('should memoize function result', () => {
         let returnValue = 5;
-        const testFunction =  (key) => returnValue;
+        const testFunction = (key) => returnValue;
 
         const memoized = memoization.memoize(testFunction, (key) => key, 1000);
         expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
@@ -17,19 +17,53 @@ describe('memoization', function () {
 
         // TODO currently fails, should work after implementing the memoize function
         expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
-
+        //original function execute again with different cache key
+        expect(memoized('b544d3ae-a72d-4755-8ce5-d25db415b776')).to.not.equal(5);
+        expect(memoized('b544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(10);
     });
     //Test Case 2
-    it('memoize function while cache key provided by resolver', () =>{
+    it('memoize function while cache key provided by resolver', () => {
         let cacheKey = 'c544d3ae';
-        const testFunction =  (key, firstValue, secondValue) => {
-            if(typeof firstValue !== 'undefined' && typeof secondValue !== 'undefined'){
+        const testFunction = (key, firstValue, secondValue) => {
+            if (typeof firstValue !== 'undefined' && typeof secondValue !== 'undefined') {
                 return firstValue + secondValue;
             }
             return key;
         }
         const memoized = memoization.memoize(testFunction, (key) => cacheKey, 1000);
         expect(memoized(cacheKey, 5, 10)).to.equal(15);
-        expect(memoized(cacheKey, 5, 10)).to.equal(15);
+        //Passing different arguments but still value return from cache as cache key detect from resolver
+        expect(memoized(cacheKey, 5, 20)).to.equal(15);
+    });
+
+    //Test Suite to test with fake timer
+    describe('Memoization check with fake timers', function () {
+        let clock;
+        beforeEach(function () {
+            clock = sinon.useFakeTimers();
+        });
+
+        afterEach(function () {
+            clock.restore();
+        });
+        it('should memoize function result until timeout exceeds', () => {
+            let returnValue = 5;
+
+            const testFunction = (key) => returnValue;
+
+            const memoized = memoization.memoize(testFunction, (key) => key, 2000);
+            expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
+            returnValue = 10;
+            clock.tick(20);
+            //Same value should return from cache
+            expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
+            clock.tick(1000);
+            //Same value should return from cache as Timeout not exceed
+            expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(5);
+            //Timeout exceeds so original function should execute again
+            clock.tick(2000);
+            expect(memoized('c544d3ae-a72d-4755-8ce5-d25db415b776')).to.equal(10);
+
+        });
     });
 });
